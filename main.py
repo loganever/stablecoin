91ee53fdac8f5f8bdda589ee6af052a2f8b7abcb
loganzzhu <1627107283@qq.com>
2023-01-12 19:40:22 +0800

    swap

--- main.py
+++ main.py
@@ -1,113 +1,26 @@
-from web3 import Web3
-from web3 import HTTPProvider
-import requests
-import json
+# -*- codeing = utf-8 -*-
 import time
-import copy
-import time
-from base import loadts2block
-from datetime import date
-import pymysql
-import csv
-import xlwt
+import flask
+from flask import Flask, request, g, jsonify
+import stablecoin
 
 
-# def insert(data):
-#     db = pymysql.connect(
-#         host="", 
-#         port=,
-#         user='root',
-#         password='',
-#         database='stable',
-#         charset='utf8mb4' 
-#         )
-#     cursor = db.cursor()
-#     sql = "insert into coin(chain,contract_address,date,coin_list,balance_list,ratio_list) values"
-#     for i in data:
-#         sql+="('%s','%s','%s','%s','%s','%s')," % (i[0],i[1].lower(),i[2],i[3],i[4],i[5])
-#     cursor.execute(sql[:-1])
-#     conn.commit()
-#     conn.close()
+app = Flask(__name__)
 
 
-def get_balance(pool):
-    pool = Web3.toChecksumAddress(pool)
-    web3 = Web3(HTTPProvider(provider))
-    abi='[{"constant":true,"inputs":[{"internalType":"address","name":"","type":"address"}],"name":"balanceOf","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"}]'
-    usdc_address = Web3.toChecksumAddress('0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48')
-    usdt_address = Web3.toChecksumAddress('0xdac17f958d2ee523a2206206994597c13d831ec7')
-    dai_address = Web3.toChecksumAddress('0x6b175474e89094c44da98b954eedeac495271d0f')
-    busd_address = Web3.toChecksumAddress('0x4Fabb145d64652a948d72533023f6E7A623C7C53')
-    dai_usdc_usdt_3crv_address = Web3.toChecksumAddress('0x6c3F90f043a72FA612cbac8115EE7e52BDe6E490')
-    usdc_contract_instance = web3.eth.contract(address=usdc_address, abi=abi)
-    usdt_contract_instance = web3.eth.contract(address=usdt_address, abi=abi)
-    dai_contract_instance = web3.eth.contract(address=dai_address, abi=abi)
-    busd_contract_instance = web3.eth.contract(address=busd_address, abi=abi)
-    dai_usdc_usdt_3crv_contract_instance = web3.eth.contract(address=dai_usdc_usdt_3crv_address, abi=abi)
-    dai = []
-    usdc = []
-    usdt = []
-    busd = []
-    dai_usdc_usdt_3crv = []
-    for key in num.keys():
-        #print(key)
-        block = int(num[key],16)
-        dai_num = dai_contract_instance.functions.balanceOf(pool).call(block_identifier=block) / 1e18
-        usdc_num = usdc_contract_instance.functions.balanceOf(pool).call(block_identifier=block) / 1e6
-        usdt_num = usdt_contract_instance.functions.balanceOf(pool).call(block_identifier=block) / 1e6
-        busd_num = busd_contract_instance.functions.balanceOf(pool).call(block_identifier=block) / 1e18
-        dai_usdc_usdt_3crv_num = dai_usdc_usdt_3crv_contract_instance.functions.balanceOf(pool).call(block_identifier=block) / 1e18
-        dai.append(float(dai_num))
-        usdc.append(float(usdc_num))
-        usdt.append(float(usdt_num))
-        busd.append(float(busd_num))
-        dai_usdc_usdt_3crv.append(float(dai_usdc_usdt_3crv_num))
-    result = []
-    return {"DAI":dai,"USDC":usdc,"USDT":usdt,"BUSD":busd,"DAI/USDC/USDT(3CRV)":dai_usdc_usdt_3crv}
+@app.get("/get_stablecoin_ratio")
+def stablecoin_ratio():
+    return stable_coin_ratio.get_data()
 
-# config
-provider = 'http://rpc'
-pool_names = ['Curve 3pool','Curve busdv2','Uni DAI-USDC-0.01%','Uni DAI-USDC-0.05%','Uni USDC-USDT-0.01%','Uni USDC-USDT-0.05%','Uni BUSD-USDC-0.05%','Uni BUSD-USDC-0.01%']
-pool_addresses = ['0xbebc44782c7db0a1a60cb6fe97d0b483032ff1c7','0x4807862aa8b2bf68830e4c8dc86d0e9a998e085a','0x5777d92f208679db4b9778590fa3cab3ac9e2168','0x6c6bc977e13df9b0de53b251522280bb72383700','0x3416cf6c708da44db2624d63ea0aaef7113527c6','0x7858e59e0c01ea06df3af3d20ac7b0003275d4bf','0x00cef0386ed94d738c8f8a74e8bfd0376926d24c','0x5e35c4eba72470ee1177dcb14dddf4d9e6d915f4']
-coin_names = [['DAI','USDC','USDT'],['BUSD','DAI/USDC/USDT(3CRV)'],['DAI','USDC'],['DAI','USDC'],['USDC','USDT'],['USDC','USDT'],['BUSD','USDC'],['BUSD','USDC']]
 
-num = loadts2block("eth","api.etherscan.io","WFBW6NI9Q4E5RSE15TH3UFPJ73PVPYDD8W",loaddays=92)
+@app.get("/get_swap_data")
+def swap_data():
+    return swap.data
 
-all_data = []
-for k in range(len(pool_addresses)):
-    pool_address = pool_addresses[k]
-    pool_name = pool_names[k]
-    coin_name = coin_names[k]
-    coin = get_balance(pool_address)
 
-    data = []
-    i = 0
-    for key in num.keys():
-        sum = 0
-        for name in coin_name:
-            sum+=coin[name][i]
-        temp = [key]
-        for name in coin_name:
-            temp.append(int(coin[name][i]))
-            temp.append(round(coin[name][i]/sum,4))
-        data.append(temp)
-        i+=1
-    all_data.append(data)
-
-wb = xlwt.Workbook()
-for k in range(len(pool_addresses)):
-    data = all_data[k]
-    pool_address = pool_addresses[k]
-    pool_name = pool_names[k]
-    coin_name = coin_names[k]
-    ws = wb.add_sheet(pool_name, cell_overwrite_ok=True)
-    ws.write(0, 0, "Date")
-    for i in range(len(coin_name)):
-        ws.write(0, 2*i+1, coin_name[i]+" Balance")
-        ws.write(0, 2*i+2, coin_name[i]+" Ratio")
-    for i in range(len(data)):
-        ws.write(i + 1, 0, str(data[i][0]))
-        for j in range(len(coin_name)):
-            ws.write(i + 1, 2*j+1, str(data[i][2*j+1]))
-            ws.write(i + 1, 2*j+2, str(data[i][2*j+2]))
-wb.save('result.xls')
+if __name__ == "__main__":
+    print("init")
+    stable_coin_ratio = stablecoin.StableCoinRatio()
+    swap = stablecoin.Swap()
+    print("ok")
+    app.run(host='0.0.0.0', port=5001)
---
+++ stablecoin.py
@@ -0,0 +1,127 @@
+# -*- codeing = utf-8 -*-
+from web3 import Web3
+from web3 import HTTPProvider
+import time
+from base import loadts2block
+import config
+import threading
+
+
+class Swap:
+
+    def __init__(self):
+        self.web3 = Web3(HTTPProvider(config.filter_provider))
+        self.fliters = []
+        self.now_block = self.web3.eth.blockNumber
+        for i in range(len(config.pool_names)):
+            host = config.pool_names[i].split(" ")[0]
+            topic = config.swap_event_topic[host]
+            address = config.pool_addresses[i]
+            self.fliters.append({"address":Web3.toChecksumAddress(address),"name":config.pool_names[i],"topic":topic,"host":host,"coins":config.coin_names[i]})
+        self.data = {}
+        thread = threading.Thread(target=self.run)
+        thread.start()
+
+    def run(self):
+        while True:
+            new_block = self.web3.eth.blockNumber
+            if new_block - self.now_block >= 1:
+                for i in range(self.now_block+1,new_block+1):
+                    self.data[i] = []
+                for i in self.fliters:
+                    filter = self.web3.eth.filter({
+                        "fromBlock": self.now_block+1,
+                        "toBlock": new_block,
+                        "address": i['address'],
+                        "topics": [
+                            i['topic']
+                        ]
+                    })
+                    log_entries = filter.get_all_entries()
+                    for j in log_entries:
+                        block = j['blockNumber']
+                        hash = j['transactionHash'].hex()
+                        address = j['address']
+                        topics = j['topics']
+                        event_data = j['data']
+                        split_event_data = []
+                        index = 2
+                        while index<len(event_data):
+                            split_event_data.append(event_data[index:index+64])
+                            index+=64
+                        for k in range(len(topics)):
+                            topics[k] = topics[k].hex()
+                        if i['host']=='Curve':
+                            volume = int(split_event_data[1],16)/config.coin[i['coins'][int(split_event_data[2],16)]]['decimal']
+                            if volume>config.threshhold:
+                                self.data[block].append({"swapFrom":i['coins'][int(split_event_data[0],16)],'swapTo':i['coins'][int(split_event_data[2],16)],'volume':volume,'transcationHash':hash,'pool_address':address,"pool_name":i['name']})
+                        elif i['host']=='Uni':
+                            volume = int(split_event_data[1],16)/config.coin[i['coins'][1]]['decimal']
+                            if volume>config.threshhold:
+                                self.data[block].append({"swapFrom":i['coins'][1],'swapTo':i['coins'][0],'volume':volume,'transcationHash':hash,'pool_address':address,"pool_name":i['name']})                    
+                self.now_block = new_block
+            keys = self.data.keys()
+            if len(keys) > 1000:
+                keys = sorted(keys)
+                for i in range(len(keys)-1000):
+                    del self.data[keys[i]]
+            time.sleep(10)
+
+
+class StableCoinRatio:
+
+    def __init__(self):
+        self.web3 = Web3(HTTPProvider(config.provider))
+        self.data_time = time.time()
+        self.data = {}
+        self.num = {}
+        self.get_stable_coin_ratio()
+
+    def get_data(self):
+        if time.time()-self.data_time>config.cache_time:
+            self.data_time = time.time()
+            thread = threading.Thread(target=self.get_stable_coin_ratio)
+            thread.start()
+        return self.data
+
+    def get_stable_coin_ratio(self):
+        num = loadts2block("eth","api.etherscan.io",config.APIKEY,loaddays=config.history_days)
+        self.num = num
+        data = {}
+        for k in range(len(config.pool_addresses)):
+            pool_address = config.pool_addresses[k]
+            pool_name = config.pool_names[k]
+            coin_name = config.coin_names[k]
+            coin = self.get_pool_data(pool_address)
+            data[pool_name] = {}
+            for name in coin_name:
+                data[pool_name][name] = {}
+            i = 0
+            for key in num.keys():
+                sum = 0
+                for name in coin_name:
+                    sum+=coin[name][i]
+                for name in coin_name:
+                    data[pool_name][name][key] = {"balance":int(coin[name][i]),"ratio":round(coin[name][i]/sum,4)}
+                i+=1
+            # for name in coin_name:
+            #     data[pool_name][name] = sorted(data[pool_name][name].items(),key=lambda s:s[0])
+        self.data = data
+
+    def get_pool_data(self,pool):
+        pool = Web3.toChecksumAddress(pool)
+        contract_instance = []
+        data = {}
+        for coin_name in config.coin.keys():
+            contract_instance.append({"instance":self.web3.eth.contract(address=Web3.toChecksumAddress(config.coin[coin_name]['address']), abi=config.balance_abi),"decimal":config.coin[coin_name]['decimal'],"name":coin_name})
+            data[coin_name] = []
+        for key in self.num.keys():
+            block = int(self.num[key],16)
+            for i in contract_instance:
+                coin_num = i['instance'].functions.balanceOf(pool).call(block_identifier=block) / i['decimal']
+                data[i['name']].append(float(coin_num))
+        return data
+
+
+if __name__ == "__main__":
+    swap = Swap()
